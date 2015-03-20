955cc6bd1da4dc81f71e84280c9d7946898641ba
kaiserklayton <claytonvioland@gmail.com>
2015-03-20 16:33:58 +0100

    Added trigram language lodeling

--- align.py
+++ align.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 # -----------------------------------------------------------------------------
-# "IBM Model 1 Translation Modeling for SMT"
+# "IBM Model 1 Translation Modeling w/ Trigram Language Modeling for SMT"
 #
-# This script defines the IBM1 class for training a probablity distribution of
-# p(s|t) values from a parallel text of source (s) and target (t) languages. The
-# IBM1 model one class is basic and uses expectation maximization as its 
-# estimation method.
+# This script defines:
+# 1. An IBM1 class for training a probablity distribution of p(s|t) values from 
+# a parallel text of source (s) and target (t) languages. The IBM Model 1 is 
+# basic and uses expectation maximization as its estimation method.
+# 2. A LanguageModel class for training a trigram language model on an input 
+# training text. 
+#
 # -----------------------------------------------------------------------------
 # Copyright (c) 2015
 #
@@ -32,11 +35,12 @@
 
 from sys import stderr
 from collections import defaultdict
+import re
 
 class IBM1:
 	"""
 	Wraps a translation model representing a distribution for p(s|t) using
-	IBM Model 1.
+	IBM Model 1. Trains on a parallel bilinguial corpus.
 	"""
 
 	def __init__(self, source, target):
@@ -56,6 +60,9 @@ class IBM1:
         		tar[i] = tar[i] / normalizer
 
 	def __getitem__(self, key):
+		"""
+		Returns the t-table probability of an alignment.
+		"""
 		return self.teetable[key]        		
 
 	def em_train(self, steps):
@@ -98,16 +105,29 @@ class IBM1:
 			# ADD NULL WORD ON SOURCE SIDE #
 			yield ([None] + src.strip().split(), tar.strip().split())
 
-	def _decode(self, source, target):
+	def align_training_data(self):
+		"""
+		Outputs alignment results for the training data.
+		"""
+		for (src,tar) in self._parallelize():
+			yield self._align(src,tar)
+
+	def show_alignment(self, alignments):
+		"""
+		Outputs the contents of a generator object as strings.
+		"""
+		for i in alignments:
+			for j in i:
+				for k in j:
+					print k,
+				print
+
+	def _align(self, source, target):
 		"""
 	   	Returns the optimal alignment given a source sentence and a target 
 	   	sentence.
 	   	"""
-	   	if type(source) == str:
-	   		source = source.upper()
-	   		source = ([None] + source.strip().split())
-	   		target = self.target
-	   	print '----------DECODING PHRASE---------->'
+	   	print '----------ALIGNING PHRASE---------->'
 		for i in source:
 			top_prob = 0
 			top_align = -1
@@ -118,29 +138,61 @@ class IBM1:
 	   				top_align = index	
 	   		yield i, target[top_align]
 
-	def decode_training_data(self):
-		"""
-		Output decoding results for the training data.
-		"""
-		for (src,tar) in self._parallelize():
-			yield self._decode(src,tar)
-
-	def show_decoding(self, alignments):
-		"""
-		Outputs the contents of a generator object as strings.
-		"""
-		for i in alignments:
-			for j in i:
-				for k in j:
-					print k,
-				print
-
-class HMM:
+class LanguageModel:
 	"""
-	Wraps a translation model representing a distribution for p(s|t) using HMMs.
+	Wraps a language model. Trains on plain text as input.
 	"""
 
-	def __init__(self, source, target):
-		self.source = source
-		self.target = target
-		self.teetable = defaultdict(lambda: defaultdict(float))
+	def __init__(self, training_data):
+		# CONVERT TRAINING DATA TEXT TO LIST OF TRIGRAMS #
+		self.training_data = training_data
+		self._get_trigrams()
+		self._wrap_lm()
+
+	def __getitem__(self, key):
+		"""
+		Returns.
+		"""
+		return self.lm[key]
+
+	def _wrap_lm(self):
+		print "INITIALIZING LANGUAGE MODEL..."
+		self.lm = {}
+		count = 0
+		for i in self.trigrams:
+			count += 1
+			if i[0] in self.lm:
+				if i[1] in self.lm[i[0]]:
+					if i[2] in self.lm[i[0]][i[1]]:
+						self.lm[i[0]][i[1]][i[2]] += 1
+					else: 
+						self.lm[i[0]][i[1]][i[2]] = 1
+				else:
+					self.lm[i[0]][i[1]] = {i[2] : 1}
+			else:
+				self.lm[i[0]] = {i[1] : {i[2]:1}}
+		return self.lm
+
+	def _get_trigrams(self):
+		"""
+		Parses training text into a list of trigrams.
+		"""
+		lines = [['<START>'] + re.sub(r'[^a-z ]+', '',						\
+					i.lower())[0:-1].split() + ['<END>'] 					\
+					for i in open(self.training_data, 'r').readlines()]
+		self.trigrams = []
+		for l in lines:
+			self.trigrams.extend(self._trigram_it(l))
+		return self.trigrams
+
+	def _trigram_it(self, line):
+		return [line[i:i+3] for i in range(len(line)-2)]
+
+	def estimate_probs(self):
+		print "ESTIMATING TRIGRAM PROBABILITIES..."
+		for a, a_index in self.lm.items():
+			for b, b_index in a_index.items():
+				total = sum(b_index.values())
+				for c in b_index.keys():
+					b_index[c] /= total
+		print "DONE.\n"
--- demo.py
+++ demo.py
@@ -3,23 +3,30 @@
 # "SMT Demo"
 # -----------------------------------------------------------------------------
 
-from align import IBM1
+from align import IBM1, LanguageModel
 
 if __name__ == '__main__':
-	# TRAIN ALIGNMENT #
+
+################## TRAIN TRANSLATION MODEL ##################
 	my_model = IBM1('/Users/clayton/Scripts/SMT/data/hansardstoy.f', \
 					'/Users/clayton/Scripts/SMT/data/hansardstoy.e')
 	my_model.em_train(10)
+
+	# TEST OUTPUT #
+#	my_model.show_alignment(my_model.align_training_data())
 #	print my_model['OUI']['YES']
 #	print my_model['IL']['HE']
 #	print my_model['IL']['IT']
 #	print my_model['NATIONALE']['NATIONAL']
-#	print my_model['CANADIENNE']['CANADIAN']
-#	print my_model['CANADIEN']['CANADIAN']
 #	print my_model['BILINGUISME']['BILINGUALISM']
 
-	# TRAIN LANGUAGE MODEL #
+################## TRAIN LANGUAGE MODEL ##################
+	my_lm = LanguageModel('/Users/clayton/Scripts/SMT/data/samp.txt')
+	my_lm.estimate_probs()
 
-	# DECODE #
-	my_decoding = my_model.decode_training_data()
-	my_model.show_decoding(my_decoding)
+	# TEST OUTPUT #
+	print my_lm['<START>']
+	print my_lm['he']
+	print my_lm['radio']
+
+################## DECODE ##################
