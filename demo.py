47a7c34b2c74bd6c266d22c95c65a5c86f3f6bc7
kaiserklayton <claytonvioland@gmail.com>
2015-03-31 21:56:44 +0200

    Translation & language modeling

--- demo.py
+++ demo.py
@@ -13,20 +13,18 @@ if __name__ == '__main__':
 	my_model.em_train(10)
 
 	# TEST OUTPUT #
-#	my_model.show_alignment(my_model.align_training_data())
-#	print my_model['OUI']['YES']
-#	print my_model['IL']['HE']
-#	print my_model['IL']['IT']
-#	print my_model['NATIONALE']['NATIONAL']
-#	print my_model['BILINGUISME']['BILINGUALISM']
+	my_model.show_alignment(my_model.align_training_data())
+	print my_model['OUI']['YES']
+	print my_model['IL']['HE']
+	print my_model['IL']['IT']
+	print my_model['NATIONALE']['NATIONAL']
+	print my_model['BILINGUISME']['BILINGUALISM']
 
 ################## TRAIN LANGUAGE MODEL ##################
-	my_lm = LanguageModel('/Users/clayton/Scripts/SMT/data/samp.txt')
+	my_lm = LanguageModel('/Users/clayton/Scripts/SMT/data/hansards.e')
 	my_lm.estimate_probs()
 
 	# TEST OUTPUT #
 	print my_lm['<START>']
 	print my_lm['he']
 	print my_lm['radio']
-
-################## DECODE ##################
